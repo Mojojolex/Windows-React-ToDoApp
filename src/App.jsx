0be67f769388cee7a37ce1df81acaf7306d49bfd
Jolex Olasiman <34479407+Mojojolex@users.noreply.github.com>
2025-05-28 04:38:50 +0800

    VS2022 Git Error : Unable to Push local files
    Need to manually upload files
    NOTE : node_modules folder is not included on this commit since it has more than a 100 files

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+    const [todos, setTodos] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+
+    const addTodo = () => {
+        if (inputValue.trim() !== '') {
+            setTodos([...todos, inputValue]);
+            setInputValue('');
+        }
+    };
+
+    const removeTodo = (index) => {
+        const newTodos = todos.filter((_, i) => i !== index);
+        setTodos(newTodos);
+    };
+
+    return (
+        <div className="App">
+            <h1>To-Do List</h1>
+            <input
+                type="text"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                placeholder="Add a new task..."
+            />
+            <button onClick={addTodo}>Add</button>
+            <ul>
+                {todos.map((todo, index) => (
+                    <li key={index}>
+                        {todo}
+                        <button onClick={() => removeTodo(index)}>Remove</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
